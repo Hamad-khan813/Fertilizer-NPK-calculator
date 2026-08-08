d27690b4e21e6a2d9288e44b70c95166182a84ca
Hamad-khan813 <akmalhuzaifa64@gmail.com>
2026-08-08 22:17:56 +0500

    new commit : Google tag Manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -195,21 +195,22 @@ export default async function RootLayout({
         />
         <JsonLd />
         {/* Google Analytics 4 — lazyOnload defers until after hydration to reduce TBT */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
-          strategy="lazyOnload"
-        />
-        <Script id="google-analytics" strategy="lazyOnload">
+        <Script id="google-tag-manager" strategy="beforeInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-XXXXXXXXXX');
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TJK3D923');
           `}
         </Script>
 
       </head>
       <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TJK3D923"
+        height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         <UnitSystemProvider initialSystem={initialSystem}>
           <a href="#main-content" className="skip-link">Skip to Content</a>
           <header role="banner">
