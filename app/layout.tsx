07fef48f1adcea76550f48cbdefe9d27c9648a10
Hamad-khan813 <akmalhuzaifa64@gmail.com>
2026-08-10 04:03:10 +0500

    adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -140,13 +140,16 @@ export default async function RootLayout({
   const initialSystem = systemCookie === 'imperial' ? 'imperial' : 'metric';
 
   return (
-    <html
+      <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5989420754611721"
-     crossorigin="anonymous"></script>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5989420754611721"
+          crossOrigin="anonymous"
+        ></script>
         <meta
           name="google-site-verification"
           content="nA6CNbhkt6FhyCDoQrXABe_G2lH00HfK4cikrh0TrWo"
